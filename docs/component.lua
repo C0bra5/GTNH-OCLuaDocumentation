7c14cd15b1569e8f63ef7e9028d94589d55c591d
Guillaume Mercier <10gui-gui10@live.ca>
2024-04-19 14:42:43 -0400

    fix me_interface not linking properly

--- docs/component.lua
+++ docs/component.lua
@@ -1,11 +1,12 @@
 --- @meta 'component'
 
+---@class Component
 local component = {
 	inventory_controller = require('components.inventory_controller'),
 	gpu = require('components.gpu'),
 	screen = require('components.screen'),
 	gt_machine = require('components.gt_machine'),
-	me_interface = require('component.me_interface')
+	me_interface = require('components.me_interface')
 };
 
 
