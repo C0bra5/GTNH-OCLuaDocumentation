192af7bbdfbacbf8fbf2ecea19cf8e5084105c19
Guillaume Mercier <C0bra5@users.noreply.github.com>
2024-12-10 13:10:48 -0500

    fix database function linkage

--- docs/components/database.lua
+++ docs/components/database.lua
@@ -8,13 +8,13 @@ local database = {}
 ---Get the representation of the item stack stored in the specified slot.
 ---@param slot integer # The slot to get an item from.
 ---@return ItemStack|nil # The item stack's descriptor if a value was found.
-function get(slot) end
+function database.get(slot) end
 
 
 ---Gets the index of an item stack with the specified hash. Returns a negative value if no such stack was found.
 ---@param hash string # The hash of the item you are looking for.
 ---@return number # slot of the item or -1 if not found
-function indexOf(hash) end
+function database.indexOf(hash) end
 
 
 ---Set an item into the specified database slot. NBT tag is expected in JSON format
@@ -24,26 +24,26 @@ function indexOf(hash) end
 ---@param nbt?    string # The nbt of the item, formatted using JSON
 ---@return boolean # True if the item was succesfully written.
 ---@return nil|string # An error telling you what went wrong.
-function set(slot, id, damage, nbt) end
+function database.set(slot, id, damage, nbt) end
 
 
 ---Clears the specified slot. Returns true if there was something in the slot before.
 ---@param slot integer
 ---@return boolean # Returns true if there was something in the slot before.
-function clear(slot) end
+function database.clear(slot) end
 
 
 ---Copies the data stored in this database to another database with the specified address.
 ---Will error if the database has empty slots.
 ---@param dbAddress string # The address of the database to copy to.
 ---@return integer # how many slots were overwritten.
-function clone(dbAddress) end
+function database.clone(dbAddress) end
 
 
 ---Computes a hash value for the item stack in the specified slot.
 ---@param slot integer # The slot to comupte the hash for
 ---@return string
-function computeHash(slot) end
+function database.computeHash(slot) end
 
 
 ---Copies an entry to another slot, optionally to another database. Returns true if something was overwritten.
@@ -51,4 +51,4 @@ function computeHash(slot) end
 ---@param toStot     integer # The slot to copy to
 ---@param dbAddress? string  # (Optional) The address of the database to copy to.
 ---@return boolean # True if somethign was overwritten.
-function copy(fromSlot, toStot, dbAddress) end
+function database.copy(fromSlot, toStot, dbAddress) end
