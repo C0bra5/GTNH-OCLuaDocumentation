092e612512c41298896dacbc7d7de3ea6e1daf02
Kogepan229 <kogepan25629@gmail.com>
2025-03-05 21:10:27 +0900

    Fix AE2 types

--- docs/type_definitions/ae_types/AECraftable.lua
+++ docs/type_definitions/ae_types/AECraftable.lua
@@ -4,10 +4,10 @@
 local AECraftable = {}
 
 ---Sends a craft request to the ME system for this pattern
----@param amount string           # The amount of items to craft
+---@param amount number           # The amount of items to craft
 ---@param prioritizePower boolean # Wether to prioririze power usage idk needs more digging?
 ---@param cpuName string          # the name of the cpu to start the craft in
----@return AECraftingJob # A tracker for the craft 
+---@return AECraftingJob # A tracker for the craft
 function AECraftable.request(amount, prioritizePower, cpuName) end
 
 
